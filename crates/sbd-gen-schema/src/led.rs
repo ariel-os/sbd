d5ef45fdd2d71a2f377608e835fae7b9eb185349
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-07-06 16:21:17 +0200

    doc: add docs and deny missing docs
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- crates/sbd-gen-schema/src/led.rs
+++ crates/sbd-gen-schema/src/led.rs
@@ -1,12 +1,20 @@
+#![deny(missing_docs)]
+//! Light emitting devices consisting of Light emitting diodes (LED).
+
 use crate::PinActive;
 use serde::{Deserialize, Serialize};
 
+/// Monocolor LED.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct MonocolorLed {
+    /// Pin of the MCU connected to the LED.
     pub pin: String,
+    /// Color of the LED.
     pub color: Option<String>,
+    /// Whether the LED is active high or low.
     pub active: Option<PinActive>,
+    /// Possible aliases of the LED.
     #[serde(default)]
     pub aliases: Vec<String>,
 }
