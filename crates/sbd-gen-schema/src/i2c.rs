92ee9d95807e4b9902c9cfa277ddde33d4efed69
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-08-12 10:46:14 +0200

    fix: drop i2c devices from the schema
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- crates/sbd-gen-schema/src/i2c.rs
+++ crates/sbd-gen-schema/src/i2c.rs
@@ -3,8 +3,9 @@
 
 use serde::{Deserialize, Serialize};
 
-/// Represents a complete I2C Bus, including the devices.
+/// Represents the MCU side of I2C Bus.
 ///
+/// Notably I2C devices are currently absent from the schema.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct I2cBus {
@@ -15,40 +16,7 @@ pub struct I2cBus {
     /// User-defined aliases for the bus.
     #[serde(default)]
     pub aliases: Vec<String>,
-    /// List of devices connected to the bus.
-    #[serde(default)]
-    pub devices: Vec<I2cDevice>,
     /// List of possible MCU I2C peripherals usable for this bus.
     #[serde(default)]
     pub possible_peripherals: Vec<String>,
-}
-
-/// A singular I2C "slave" device.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-pub struct I2cDevice {
-    /// Device type.
-    ///
-    /// This is supposed to be an *identifier*.
-    /// In the Ariel OS generator, this will set the flag `has_i2c_device_<type>`.
-    #[serde(rename = "type")]
-    pub type_: String,
-
-    /// Part name of the device coming from the supplier.
-    ///
-    /// If given, this can be used to automatically generate
-    /// the required initialization code.
-    /// Right now, this is only possible for sensors for which there is
-    /// an `ariel-os-sensor-x` crate.
-    #[serde(default)]
-    pub part_number: Option<String>,
-    /// Device address on bus in hex.
-    pub address: String,
-
-    /// Optional description.
-    ///
-    /// This is mainly meant for devices that aren't supported
-    /// by Ariel OS
-    #[serde(default)]
-    pub description: Option<String>,
 }
